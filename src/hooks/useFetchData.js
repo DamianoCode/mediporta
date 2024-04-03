516776e7d1d6d206ba46608d129d2940d737dde6
DamianoCode <dam1138@gmail.com>
2024-04-03 19:42:56 +0200

    add fetch data hook

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+const useFetchData = (initialUrl) => {
+  const [data, setData] = useState({});
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(url, { signal });
+        const json = await response.json();
+
+        setData(json);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+
+    return () => controller.abort();
+  }, [url]);
+
+  return [{ data, isLoading, isError }, setUrl];
+};
+
+export default useFetchData;
